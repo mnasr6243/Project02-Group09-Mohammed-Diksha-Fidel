b7918197ae6264f39740b63573806ee7ba664d91
Fidel G Vazquez <fidgvaz@gmail.com>
2025-11-16 09:07:04 -0800

    created landing page with basic layout, admin visibility, and logout

---
+++ app/src/main/java/com/example/project02_group09_mohammed_diksha_fidel/LandingPageActivity.java
@@ -0,0 +1,45 @@
+package com.example.project02_group09_mohammed_diksha_fidel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project02_group09_mohammed_diksha_fidel.session.SessionManager;
+
+public class LandingPageActivity extends AppCompatActivity {
+    private SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+        session = new SessionManager(this);
+
+
+
+        TextView tv = findViewById(R.id.tvWelcome);
+        Button btnAdmin = findViewById(R.id.btnAdmin);
+        Button btnLogout = findViewById(R.id.btnLogout);
+
+        tv.setText("Welcome, " + session.username());
+
+        if (session.isAdmin()) {
+            btnAdmin.setVisibility(View.VISIBLE);
+        }
+
+        btnAdmin.setOnClickListener(v ->
+                startActivity(new Intent(this, AdminManagerActivity.class))
+        );
+
+        btnLogout.setOnClickListener(v -> {
+            session.clear();
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+    }
+}

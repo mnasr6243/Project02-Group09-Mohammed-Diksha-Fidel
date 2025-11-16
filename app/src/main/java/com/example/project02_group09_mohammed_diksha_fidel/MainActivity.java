0e2d70fe0cc18bdcca99346ebca4b15fb8c61b80
Fidel G Vazquez <fidgvaz@gmail.com>
2025-11-15 16:46:47 -0800

    Added TODO comment for Admin Manager + Challenges feature

--- app/src/main/java/com/example/project02_group09_mohammed_diksha_fidel/MainActivity.java
+++ app/src/main/java/com/example/project02_group09_mohammed_diksha_fidel/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: Implement Admin Manager + Challenges feature heres
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

61e99d6153c88a8049c49a0bc4fe36a531717ab6
Fidel G Vazquez <fidgvaz@gmail.com>
2025-11-28 19:07:13 -0800

    Define Room User entity with admin flag

--- app/src/main/java/com/example/project02_group09_mohammed_diksha_fidel/data/User.java
+++ app/src/main/java/com/example/project02_group09_mohammed_diksha_fidel/data/User.java
@@ -1,18 +1,24 @@
 package com.example.project02_group09_mohammed_diksha_fidel.data;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "user")
 public class User {
+
     @PrimaryKey(autoGenerate = true)
     public int id;
 
+    @NonNull
     public String username;
+
+    @NonNull
     public String password;
+
     public boolean isAdmin;
 
-    public User(String username, String password, boolean isAdmin) {
+    public User(@NonNull String username, @NonNull String password, boolean isAdmin) {
         this.username = username;
         this.password = password;
         this.isAdmin = isAdmin;
